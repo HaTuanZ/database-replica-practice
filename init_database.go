b7dadae4e3a2da79717dd29a38d8d2f21e3c4330
HaTuanZ <hatuanzzz85@gmail.com>
2024-08-03 12:51:10 +0700

    :+1: Add the logic to generate the configuration files

--- init_database.go
+++ init_database.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"math/big"
 	"os"
+	"path/filepath"
 	"strconv"
 
 	"github.com/joho/godotenv"
@@ -49,27 +50,37 @@ func main() {
 
 	for i := 1; i <= masterReplica; i++ {
 		serviceName := fmt.Sprintf("db-master-%d", i)
+		configFileName := createConfigFile(fmt.Sprintf("my-master-%d.conf", i), i)
+
 		compose.Services[serviceName] = Service{
 			Image: "mysql:8.4",
 			Environment: map[string]string{
 				"MYSQL_ROOT_PASSWORD": generatePassword(),
 				"MYSQL_DATABASE":      "example_db",
 			},
-			Ports:   []string{fmt.Sprintf("%d:3306", 3306+1)},
-			Volumes: []string{fmt.Sprintf("./mysql-data-master-%d:/var/lib/mysql", i)},
+			Ports: []string{fmt.Sprintf("%d:3306", 3306+1)},
+			Volumes: []string{
+				fmt.Sprintf("./mysql-data-master-%d:/var/lib/mysql", i),
+				fmt.Sprintf("./%s:/etc/mysql/conf.d/my.cnf", configFileName),
+			},
 		}
 	}
 
 	for i := 1; i <= slaveReplicas; i++ {
 		serviceName := fmt.Sprintf("db-slave-%d", i)
+		configFileName := createConfigFile(fmt.Sprintf("my-slave-%d.conf", i), i)
+
 		compose.Services[serviceName] = Service{
 			Image: "mysql:8.4",
 			Environment: map[string]string{
 				"MYSQL_ROOT_PASSWORD": generatePassword(),
 				"MYSQL_DATABASE":      "example_db",
 			},
-			Ports:   []string{fmt.Sprintf("%d:3306", 3306+i+masterReplica)},
-			Volumes: []string{fmt.Sprintf("./mysql-data-slave-%d:/var/lib/mysql", 1)},
+			Ports: []string{fmt.Sprintf("%d:3306", 3306+i+masterReplica)},
+			Volumes: []string{
+				fmt.Sprintf("./mysql-data-slave-%d:/var/lib/mysql", 1),
+				fmt.Sprintf("./%s:/etc/mysql/conf.d/my.cnf", configFileName),
+			},
 		}
 	}
 
@@ -97,4 +108,30 @@ func generatePassword() string {
 	}
 
 	return string(password)
+}
+
+func createConfigFile(fileName string, serverId int) string {
+	config := fmt.Sprintf(`[mysqld]
+server-id=%d
+log_bin=mysql-bin
+binlog_format=ROW
+	`, serverId)
+
+	filePath := "conf/" + fileName
+
+	dir := filepath.Dir(filePath)
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		err := os.MkdirAll(dir, os.ModePerm)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	err := os.WriteFile(filePath, []byte(config), 0644)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return filePath
 }
